41634b52e2599d0800c13c56374bf2ea38ed81df
jetpackpony <jetpackpony@gmail.com>
2019-11-26 00:37:00 +0300

    Add extracts for Cutsoms-Main

--- store/mapData/CustomsMain.js
+++ store/mapData/CustomsMain.js
@@ -5,21 +5,235 @@ const mapData = {
   imgPath: customsMainImg,
   extracts: [
     {
-      id: "ext-1",
+      id: "ext-18",
       names: {
-        en: "test-1",
-        ru: "тест-1"
+        en: "ZB-1011",
+        ru: "ЗБ-1011"
       },
-      coords: { x: 100, y: 100 }
+      faction: "pmc",
+      specialConditions: null,
+      coords: { x: 1815, y: 870 }
+    },
+    {
+      id: "ext-15",
+      names: {
+        en: "ZB-1012",
+        ru: "ЗБ-1012"
+      },
+      faction: "pmc",
+      specialConditions: "??? когда горит фонарь",
+      coords: { x: 1540, y: 820 }
+    },
+    {
+      id: "ext-12",
+      names: {
+        en: "Old Gas Station",
+        ru: "Старая Заправка"
+      },
+      faction: "pmc",
+      specialConditions: "??? когда горит зеленая дымовая шашка",
+      coords: { x: 1255, y: 955 }
     },
     {
       id: "ext-2",
       names: {
-        en: "test-2",
-        ru: "тест-2"
+        en: "Trailer Park",
+        ru: "Трейлерный Парк"
       },
-      coords: { x: 200, y: 200 }
-    }
+      faction: "pmc",
+      specialConditions: null,
+      coords: { x: 190, y: 1035 }
+    },
+    {
+      id: "ext-6",
+      names: {
+        en: "Smuggler's Boat",
+        ru: "Лодка Контрабандиста"
+      },
+      faction: "pmc",
+      specialConditions: "??? когда горит костер",
+      coords: { x: 660, y: 430 }
+    },
+    {
+      id: "ext-10",
+      names: {
+        en: "Dorms V-Ex",
+        ru: "А-Выход у Общаги"
+      },
+      faction: "pmc",
+      specialConditions: "??? 3000 Руб, ждать 1 минуту, одноразовый",
+      coords: { x: 1080, y: 290 }
+    },
+    {
+      id: "ext-1",
+      names: {
+        en: "Crossroads",
+        ru: "Перекресток"
+      },
+      faction: "all",
+      specialConditions: null,
+      coords: { x: 170, y: 750 }
+    },
+    {
+      id: "ext-7",
+      names: {
+        en: "RUAF Roadblock",
+        ru: "Блокпост ВС РФ"
+      },
+      faction: "all",
+      specialConditions: "??? когда горит фонарь",
+      coords: { x: 715, y: 915 }
+    },
+    {
+      id: "ext-22",
+      names: {
+        en: "Old Road Gate",
+        ru: "Ворота на Старой Дороге"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1080, y: 290 }
+    },
+    {
+      id: "ext-3",
+      names: {
+        en: "Trailer Park Workers' Shack",
+        ru: "Времянка у трейл. Парка"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 325, y: 1045 }
+    },
+    {
+      id: "ext-4",
+      names: {
+        en: "Railroad To Tarkov",
+        ru: "ЖД до Таркова"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 470, y: 1015 }
+    },
+    {
+      id: "ext-5",
+      names: {
+        en: "Railroad to Port",
+        ru: "ЖД к Порту"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 485, y: 545 }
+    },
+    {
+      id: "ext-8",
+      names: {
+        en: "Sniper Roadblock",
+        ru: "Блокпост Снайперов"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 755, y: 430 }
+    },
+    {
+      id: "ext-9",
+      names: {
+        en: "Warehouse 17",
+        ru: "Склад 17"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 810, y: 770 }
+    },
+    {
+      id: "ext-11",
+      names: {
+        en: "Factory Shacks",
+        ru: "Заводские Времянки"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1095, y: 615 }
+    },
+    {
+      id: "ext-23",
+      names: {
+        en: "Old Gas Station",
+        ru: "Старая Заправка"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1225, y: 990 }
+    },
+    {
+      id: "ext-13",
+      names: {
+        en: "Warehouse 4",
+        ru: "Склад 4"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1305, y: 680 }
+    },
+    {
+      id: "ext-14",
+      names: {
+        en: "Railroad To Military Base",
+        ru: "ЖД к Военной Базе"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1540, y: 300 }
+    },
+    {
+      id: "ext-16",
+      names: {
+        en: "Passage between rocks",
+        ru: "Проход между скалами"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1635, y: 315 }
+    },
+    {
+      id: "ext-17",
+      names: {
+        en: "Military Base CP",
+        ru: "Времянка"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1740, y: 365 }
+    },
+    {
+      id: "ext-19",
+      names: {
+        en: "Scavs checkpoint",
+        ru: "КПП Диких"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1850, y: 690 }
+    },
+    {
+      id: "ext-20",
+      names: {
+        en: "Administration Gate",
+        ru: "Административные Ворота"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1870, y: 730 }
+    },
+    {
+      id: "ext-21",
+      names: {
+        en: "Factory Far Corner",
+        ru: "Дальний Угол Завода"
+      },
+      faction: "scav",
+      specialConditions: null,
+      coords: { x: 1840, y: 940 }
+    },
   ]
 };
 

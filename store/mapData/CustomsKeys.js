193132b2fc7029c8273a568857b83a8b23fa3061
jetpackpony <jetpackpony@gmail.com>
2019-11-26 00:41:51 +0300

    Update customs keys map

--- store/mapData/CustomsKeys.js
+++ store/mapData/CustomsKeys.js
@@ -3,24 +3,7 @@ import customsKeysImg from '../../mapImages/Customs-Keys.jpg';
 const mapData = {
   title: "Customs Keys",
   imgPath: customsKeysImg,
-  extracts: [
-    {
-      id: "ext-1",
-      names: {
-        en: "test-1",
-        ru: "тест-1"
-      },
-      coords: { x: 100, y: 100 }
-    },
-    {
-      id: "ext-2",
-      names: {
-        en: "test-2",
-        ru: "тест-2"
-      },
-      coords: { x: 200, y: 200 }
-    }
-  ]
+  extracts: []
 };
 
 export default mapData;

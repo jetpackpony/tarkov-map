63ac5c444c1518eac8d4ac2d2ef2c82fda98cc1e
jetpackpony <jetpackpony@gmail.com>
2019-11-25 18:31:33 +0300

    Split mapdata into several files

---
+++ store/mapData/CustomsKeys.js
@@ -0,0 +1,24 @@
+import customsKeysImg from '../../mapImages/Customs-Keys.jpg';
+
+const mapData = {
+  title: "Customs Keys",
+  imgPath: customsKeysImg,
+  extracts: [
+    {
+      names: {
+        en: "test-1",
+        ru: "тест-1"
+      },
+      coords: { x: 100, y: 100 }
+    },
+    {
+      names: {
+        en: "test-2",
+        ru: "тест-2"
+      },
+      coords: { x: 200, y: 200 }
+    }
+  ]
+};
+
+export default mapData;
---
+++ store/mapData/CustomsMain.js
@@ -0,0 +1,26 @@
+import customsMainImg from '../../mapImages/Customs.png';
+
+const mapData = {
+  title: "Customs Main",
+  imgPath: customsMainImg,
+  extracts: [
+    {
+      id: "ext-1",
+      names: {
+        en: "test-1",
+        ru: "тест-1"
+      },
+      coords: { x: 100, y: 100 }
+    },
+    {
+      id: "ext-1",
+      names: {
+        en: "test-2",
+        ru: "тест-2"
+      },
+      coords: { x: 200, y: 200 }
+    }
+  ]
+};
+
+export default mapData;

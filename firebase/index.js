26e3e5fd25376c7e033bf7a49a21d3ba3829811b
jetpackpony <jetpackpony@gmail.com>
2020-02-11 16:54:53 +0300

    Add scripts for main and demo build trargets

--- firebase/index.js
+++ firebase/index.js
@@ -5,7 +5,7 @@ import firebaseConfig from './config';
 const initFirebase = () => {
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
-  const mapObjectsRef = db.collection("mapObjects");
+  const mapObjectsRef = db.collection(process.env.DB_COLLECTION_NAME || "mapObjectsDev");
   const listeners = [];
 
   // Sub to updates

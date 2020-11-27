600757f3eec8cc50a78f8646d8b5a71fe7ee4a5f
jetpackpony <jetpackpony@gmail.com>
2020-11-27 18:49:44 +0300

    Update interchange map and extracts

--- store/mapData/InterchangeMain.js
+++ store/mapData/InterchangeMain.js
@@ -20,7 +20,7 @@ const mapData = {
       },
       faction: "all",
       specialConditions: null,
-      coords: { x: 166, y: 188 }
+      coords: {x: 195, y: 217}
     },
     {
       id: "ext-2",
@@ -30,7 +30,7 @@ const mapData = {
       },
       faction: "all",
       specialConditions: null,
-      coords: { x: 747, y: 777 }
+      coords:  {x: 895, y: 925}
     },
     {
       id: "ext-3",
@@ -43,8 +43,48 @@ const mapData = {
         en: "??? 3000 Rub, 1 time use",
         ru: "??? 3000 руб, одноразовый"
       },
-      coords: { x: 683, y: 262 }
-    }
+      coords: {x: 817, y: 310}
+    },
+    {
+      id: "ext-4",
+      names: {
+        en: "Hole in fence",
+        ru: "Дыра в заборе"
+      },
+      faction: "pmc",
+      specialConditions: {
+        en: "??? only without a backpack",
+        ru: "??? только без рюкзака"
+      },
+      coords: {x: 792, y: 606}
+    },
+    {
+      id: "ext-5",
+      names: {
+        en: "Scav camp",
+        ru: "Лагерь диких"
+      },
+      faction: "all",
+      specialConditions: {
+        en: "??? you need to be a scav + pmc team to extract",
+        ru: "??? нужна пара ЧВК + Дикий чтобы выйти"
+      },
+      coords: {x: 335, y: 642}
+    },
+    {
+      id: "ext-6",
+      names: {
+        en: "Saferoom Exfil",
+        ru: "Безопасная комната"
+      },
+      faction: "pmc",
+      specialConditions: {
+        en: "??? pull the lever at the power plant + flush a toilet at Burger Spot + swipe card #11SR",
+        ru: "??? нажать рычаг на электростанции + смыть унитаз в Burger Spot + провесит картой #11SR"
+      },
+      coords: {x: 641, y: 1558},
+      activationCoords: {x: 2502, y: 772}
+    },
   ]
 };
 

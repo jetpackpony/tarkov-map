cef572a02899db00390e7d0265bb818ab7c3fd5a
jetpackpony <jetpackpony@gmail.com>
2022-08-02 13:09:28 +0300

    Add Object.keys and Object.entries overloads

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,16 @@ declare module '*.webp' {
   const content: string;
   export default content;
 }
+
+interface ObjectConstructor {
+  keys<T>(o: T):
+    T extends object ? (keyof T)[] :
+    T extends number ? [] :
+    T extends Array<any> | string ? string[] :
+    never,
+  entries<T, V>(o: T):
+    T extends object ? [(keyof T), T[keyof T]][] :
+    T extends number ? [] :
+    T extends ArrayLike<V> ? [string, V][] :
+    never,
+};

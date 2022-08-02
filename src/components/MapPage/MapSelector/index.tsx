d92e401550832ca85c8b18e38492ed423b50ca61
jetpackpony <jetpackpony@gmail.com>
2022-08-02 13:15:54 +0300

    Add types for MapSelector

--- src/components/MapPage/MapSelector/index.tsx
+++ src/components/MapPage/MapSelector/index.tsx
@@ -1,27 +1,45 @@
-import { h } from 'preact';
-import mapData from '../../../store/mapData';
+import { ComponentChildren, h } from 'preact';
+import mapData, { MapName } from '../../../store/mapData';
 import './mapSelector.css';
-import { useTranslation } from 'react-i18next';
+import { MapPageProps } from '../MapPage';
+import { getCurrentLang, Language } from '../../../i18n';
+import { isEnum, MapGroupId } from '../../../types';
+import { TargetedEvent } from 'preact/compat';
 
-const mapObject = (f, obj) => (
-  Object.keys(obj).map((key) => f(key, obj[key], obj))
-);
-
-const getMapGroups = (lang) => {
-  return Object.keys(mapData.maps).reduce((acc, key) => {
-    const map = mapData.maps[key];
-    if (!acc[map.groupId]) {
-      acc[map.groupId] = {
-        groupName: map.groupName[lang],
-        maps: {}
-      };
-    }
-    acc[map.groupId].maps[key] = map.title[lang];
-    return acc;
-  }, {});
+type MapGroup = {
+  groupName: string,
+  maps: {
+    [key in MapName]?: string
+  }
 };
 
-const MapOptGroup = ({ label, children }) => {
+type MapGroups = {
+  [key in MapGroupId]: MapGroup
+};
+
+const getMapGroups = (lang: Language): MapGroups => {
+  return Object.values(MapName).reduce(
+    (acc, mapName) => {
+      const map = mapData.maps[mapName];
+      if (!acc[map.groupId]) {
+        acc[map.groupId] = {
+          groupName: map.groupName[lang],
+          maps: {}
+        };
+      }
+      acc[map.groupId].maps[mapName] = map.title[lang];
+      return acc;
+    },
+    {} as MapGroups
+  );
+};
+
+interface MapOptGroupProps {
+  label: string,
+  children: ComponentChildren
+};
+
+const MapOptGroup = ({ label, children }: MapOptGroupProps) => {
   return (
     <optgroup label={label}>
       {children}
@@ -29,7 +47,13 @@ const MapOptGroup = ({ label, children }) => {
   );
 };
 
-const MapOption = ({ mapId, name, isSelected }) => (
+interface MapOptionProps {
+  mapId: MapName,
+  name: string,
+  isSelected: boolean
+};
+
+const MapOption = ({ mapId, name, isSelected }: MapOptionProps) => (
   <option
     value={mapId}
     selected={isSelected}
@@ -38,34 +62,37 @@ const MapOption = ({ mapId, name, isSelected }) => (
   </option>
 );
 
-const MapSelector = ({ currentMap, onMapSelected }) => {
-  const { i18n } = useTranslation();
-  const mapGroups = getMapGroups(i18n.language);
+interface MapSelectorProps {
+  currentMap: MapName,
+  onMapSelected: MapPageProps['onMapSelected']
+};
+
+const MapSelector = ({ currentMap, onMapSelected }: MapSelectorProps) => {
+  const mapGroups = getMapGroups(getCurrentLang());
+  const onChange = (e: TargetedEvent<HTMLSelectElement, Event>) => {
+    if (e && isEnum(MapName)(e.currentTarget.value)) {
+      onMapSelected(e.currentTarget.value)
+    }
+  };
   return (
     <select
       id="map-select"
-      onChange={(e) => onMapSelected(e.target.value)}
+      onChange={onChange}
     >
       {
-        mapObject(
-          (_, group) => (
-            <MapOptGroup label={group.groupName}>
-              {
-                mapObject(
-                  (mapId, name) => (
-                    <MapOption
-                      mapId={mapId}
-                      name={`${group.groupName} - ${name}`}
-                      isSelected={mapId === currentMap}
-                    />
-                  ),
-                  group.maps
-                )
-              }
-            </MapOptGroup>
-          ),
-          mapGroups
-        )
+        Object.values(mapGroups).map((group) => (
+          <MapOptGroup label={group.groupName}>
+            {
+              Object.entries(group.maps).map(([mapId, name]) => (
+                <MapOption
+                  mapId={mapId}
+                  name={`${group.groupName} - ${name}`}
+                  isSelected={mapId === currentMap}
+                />
+              ))
+            }
+          </MapOptGroup>
+        ))
       }
     </select>
   );

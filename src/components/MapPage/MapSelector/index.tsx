7814f9727bd7b19907e57d73d9b2565460e16ba2
jetpackpony <jetpackpony@gmail.com>
2022-08-05 12:06:42 +0300

    Migrate MapSelector to use redux toolkit

--- src/components/MapPage/MapSelector/index.tsx
+++ src/components/MapPage/MapSelector/index.tsx
@@ -1,102 +1,17 @@
-import { ComponentChildren, h } from 'preact';
-import mapData, { MapName } from '../../../store/mapData';
-import './mapSelector.css';
-import { MapPageProps } from '../MapPage';
-import { isEnum, MapGroupId } from '../../../types';
-import { TargetedEvent } from 'preact/compat';
-import { useLanguageContext, Language } from '../../../I18nContext';
+import { compose } from "rambda";
+import { useAppDispatch, useAppSelector } from "../../../store";
+import { selectCurrentMap, selectMap } from "../../../store/uiSlice";
+import MapSelector from "./MapSelector";
 
-type MapGroup = {
-  groupName: string,
-  maps: {
-    [key in MapName]?: string
-  }
-};
+export default () => {
+  const currentMap = useAppSelector(selectCurrentMap);
+  const dispatch = useAppDispatch();
+  const onMapSelected = compose(dispatch, selectMap);
 
-type MapGroups = {
-  [key in MapGroupId]: MapGroup
-};
-
-const getMapGroups = (lang: Language): MapGroups => {
-  return Object.values(MapName).reduce(
-    (acc, mapName) => {
-      const map = mapData.maps[mapName];
-      if (!acc[map.groupId]) {
-        acc[map.groupId] = {
-          groupName: map.groupName[lang],
-          maps: {}
-        };
-      }
-      acc[map.groupId].maps[mapName] = map.title[lang];
-      return acc;
-    },
-    {} as MapGroups
-  );
-};
-
-interface MapOptGroupProps {
-  label: string,
-  children: ComponentChildren
-};
-
-const MapOptGroup = ({ label, children }: MapOptGroupProps) => {
   return (
-    <optgroup label={label}>
-      {children}
-    </optgroup>
-  );
+    <MapSelector
+      currentMap={currentMap}
+      onMapSelected={onMapSelected}
+    />
+  )
 };
-
-interface MapOptionProps {
-  mapId: MapName,
-  name: string,
-  isSelected: boolean
-};
-
-const MapOption = ({ mapId, name, isSelected }: MapOptionProps) => (
-  <option
-    value={mapId}
-    selected={isSelected}
-  >
-    {name}
-  </option>
-);
-
-interface MapSelectorProps {
-  currentMap: MapName,
-  onMapSelected: MapPageProps['onMapSelected']
-};
-
-const MapSelector = ({ currentMap, onMapSelected }: MapSelectorProps) => {
-  const { getCurrentLang } = useLanguageContext();
-  const mapGroups = getMapGroups(getCurrentLang());
-  const onChange = (e: TargetedEvent<HTMLSelectElement, Event>) => {
-    if (e && isEnum(MapName)(e.currentTarget.value)) {
-      onMapSelected({ mapId: e.currentTarget.value })
-    }
-  };
-  return (
-    <select
-      id="map-select"
-      onChange={onChange}
-    >
-      {
-        Object.values(mapGroups).map((group) => (
-          <MapOptGroup label={group.groupName}>
-            {
-              Object.entries(group.maps).map(([mapId, name]) => (
-                <MapOption
-                  mapId={mapId}
-                  name={`${group.groupName} - ${name}`}
-                  isSelected={mapId === currentMap}
-                />
-              ))
-            }
-          </MapOptGroup>
-        ))
-      }
-    </select>
-  );
-};
-
-export default MapSelector;

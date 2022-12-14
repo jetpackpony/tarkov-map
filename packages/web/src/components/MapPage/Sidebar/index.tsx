64f46121c90e6fb6e80e26bb287c30550e414242
jetpackpony <jetpackpony@gmail.com>
2022-12-14 14:04:45 +0300

    Add a close button to the sidebar

--- packages/web/src/components/MapPage/Sidebar/index.tsx
+++ packages/web/src/components/MapPage/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { ComponentChildren, h } from "preact";
+import Button from "../Button/Button";
 import styles from "./sidebar.module.css";
 
 interface SidebarProps {
@@ -14,7 +15,13 @@ const Sidebar = ({ children, headerElement, isOpen, close }: SidebarProps) => {
   return (
     <div onMouseLeave={close} class={classes.join(" ")}>
       <div class={styles.container}>
-        <header class={styles.header}>{headerElement}</header>
+        <header class={styles.header}>
+          {headerElement}
+          <Button className={styles.closeButton} onClick={close}>
+            <div />
+            <div />
+          </Button>
+        </header>
         <div class={styles.sidebarContent}>{children}</div>
       </div>
     </div>

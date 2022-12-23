3cc7df118d5fb9567c508c1722753b868e39a799
jetpackpony <jetpackpony@gmail.com>
2022-12-23 16:01:44 +0300

    Add svg support to the project

--- packages/web/src/custom.d.ts
+++ packages/web/src/custom.d.ts
@@ -3,6 +3,11 @@ declare module "*.webp" {
   export default content;
 }
 
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
 declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
